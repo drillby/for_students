e1c80aeb570fd55a4300130d9fb5db4c06e78b47
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2022-11-25 13:13:02 +0100

    Add files via upload

---
+++ ES_2019/php/echo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Nadpis</h1>";
+        echo "<a href='https://www.google.com'>Odkaz</a>";
+    ?>
+    <table>
+        <tr>
+            <td>data1</td>
+            <td>data2</td>
+        </tr>
+    </table>
+    <?php
+    echo "<h1>Nadpis</h1>";
+    ?>
+</body>
+</html>
