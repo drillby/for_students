432403612c3ac0811ec07eeba85548f038629c2b
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2022-11-30 12:13:59 +0100

    Add files via upload

---
+++ EP_2021/Python/zakladni_syntax.py
@@ -0,0 +1,35 @@
+promena_1 = 26 # komentář
+promena_2 = "text"
+promena_3 = False
+promena_4 = 26.59
+
+viceslovna_promenna = ...
+# promena_1 = "asd"
+
+print(26)
+print("text")
+print(promena_3)
+
+print(26/8)
+print(26//8) # celočíselné dělení
+
+print("text_1" + "text_2")
+
+print(26);
+print(promena_1 + promena_4) # sřítání celého a desetiného čísla 
+
+#promena_1 + promena_2
+
+promena_5 = "258"
+print(type(promena_5))
+
+promena_5 = int(promena_5)
+print(type(promena_5))
+
+promena_5 = str(promena_5)
+
+promena_4 = str(promena_4)
+promena_4 = float(promena_4)
+
+user_input = input("Něco napiš... ")
+print(user_input, type(user_input))
