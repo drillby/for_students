13db753a0a5ad9f3f956ee4f0a53301b3c6eb63a
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2023-09-26 11:14:04 +0200

    Delete ES_2019 directory

--- ES_2019/eshop_es4/conifg.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "dbs.spskladno.cz";
-$username = "student22";
-$password = "spsnet";
-$dbname = "vyuka22";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn)
-        die("Nepodařilo se navázat spojení se serverem. Zkuste to později.");
