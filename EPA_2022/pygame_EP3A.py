03829d973e8d083bb930952006466ee9ed4bb2e5
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2024-10-11 12:10:42 +0200

    Update pygame_EP3A.py

--- EPA_2022/pygame_EP3A.py
+++ EPA_2022/pygame_EP3A.py
@@ -1,4 +1,7 @@
-import math
+# TODO: zrychlit pohyb hada když skore%10 == 0
+# TODO: vypsat skore na obrazovku
+
+import random
 
 import pygame
 
@@ -9,41 +12,59 @@ OKNO_SIRKA = 800
 
 BILA = (255, 255, 255)
 MODRA = (0, 0, 255)
+CERVENA = (255, 0, 0)
 
 hodiny = pygame.time.Clock()
 FPS = 60
 
+MOVE_SNAKE_SEC = 0.5
+MOVE_SNAKE_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(MOVE_SNAKE_EVENT, int(MOVE_SNAKE_SEC * 1000))
 
-class Hrac:
-    def __init__(self, rozmer, pozice, barva, rychlost=5):
+skore = 0
+
+
+class GameObject:
+    def __init__(self, rozmer, pozice, barva):
         self.rozmer = rozmer
         self.pozice = pozice
         self.barva = barva
-        self.rychlost = rychlost
 
     def vykresli(self, okno):
         pygame.draw.rect(okno, self.barva, (*self.pozice, *self.rozmer))
-        # pygame.draw.rect(okno, self.barva, (self.pozice[0], self.pozice[1], self.rozmer[0], self.rozmer[1]))
 
-    def pohyb(self, stisknute_klavesy):
-        move_x = 0
-        move_y = 0
+
+class Apple(GameObject):
+    def __init__(self, rozmer, pozice, barva):
+        super().__init__(rozmer, pozice, barva)
+
+    def zkontroluj_kolizi(self, hrac):
+        return hrac.pozice == self.pozice
+
+
+class Snake(GameObject):
+    def __init__(self, rozmer, pozice, barva):
+        super().__init__(rozmer, pozice, barva)
+        self.smer_pohybu = [0, 0]  # [x, y]
+
+    def zjisteni_smeru(self, stisknute_klavesy):
         if stisknute_klavesy[pygame.K_LEFT]:
-            move_x = -self.rychlost
-        if stisknute_klavesy[pygame.K_RIGHT]:
-            move_x = self.rychlost
+            self.smer_pohybu[0] = -1
+            self.smer_pohybu[1] = 0
+        elif stisknute_klavesy[pygame.K_RIGHT]:
+            self.smer_pohybu[0] = 1
+            self.smer_pohybu[1] = 0
+
         if stisknute_klavesy[pygame.K_UP]:
-            move_y = -self.rychlost
-        if stisknute_klavesy[pygame.K_DOWN]:
-            move_y = self.rychlost
+            self.smer_pohybu[1] = -1
+            self.smer_pohybu[0] = 0
+        elif stisknute_klavesy[pygame.K_DOWN]:
+            self.smer_pohybu[1] = 1
+            self.smer_pohybu[0] = 0
 
-        direction = [move_x, move_y]
-        if abs(direction[0]) == self.rychlost and abs(direction[1]) == self.rychlost:
-            direction[0] *= 1 / math.sqrt(2)
-            direction[1] *= 1 / math.sqrt(2)
-
-        self.pozice[0] += direction[0]
-        self.pozice[1] += direction[1]
+    def pohyb(self):
+        self.pozice[0] += self.smer_pohybu[0] * self.rozmer[0]
+        self.pozice[1] += self.smer_pohybu[1] * self.rozmer[1]
 
     def zkontroluj_hranice(self):
         if self.pozice[0] < 0:
@@ -62,21 +83,31 @@ okno = pygame.display.set_mode((OKNO_SIRKA, OKNO_VYSKA))
 # Nastavení titulku okna
 pygame.display.set_caption("Můj první program v Pygame")
 
-H1 = Hrac((50, 50), [100, 100], MODRA)
+H1 = Snake((50, 50), [100, 100], MODRA)
+A1 = Apple((50, 50), [200, 200], CERVENA)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == MOVE_SNAKE_EVENT:
+            H1.pohyb()
+            H1.zkontroluj_hranice()
 
     stisknute_klavesy = pygame.key.get_pressed()
 
-    H1.pohyb(stisknute_klavesy)
-    H1.zkontroluj_hranice()
+    H1.zjisteni_smeru(stisknute_klavesy)
+
+    if A1.zkontroluj_kolizi(H1):
+        skore += 1
+        seznam_x = [i for i in range(0, OKNO_SIRKA, A1.rozmer[0])]
+        seznam_y = list(range(0, OKNO_VYSKA, A1.rozmer[1]))
+        A1.pozice = [random.choice(seznam_x), random.choice(seznam_y)]
 
     okno.fill(BILA)
     # Obnovení okna
     H1.vykresli(okno)
+    A1.vykresli(okno)
     pygame.display.flip()
     hodiny.tick(FPS)
