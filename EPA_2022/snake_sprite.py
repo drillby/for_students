58c18e79ea776c74cdfa5f5e9d98e6c411d39cbf
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2024-11-08 12:23:52 +0100

    Update snake_sprite.py

--- EPA_2022/snake_sprite.py
+++ EPA_2022/snake_sprite.py
@@ -1,4 +1,4 @@
-import random
+# přidat jablko
 
 import pygame
 
@@ -31,31 +31,40 @@ game_over = False
 
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self, rozmer, pozice, barva, barva_hlavy):
+    def __init__(self, rozmer, pozice):
         super().__init__()
         self.rozmer = rozmer
         self.smer_pohybu = [0, 0]
-        self.barva = barva
-        self.barva_hlavy = barva_hlavy
         self.pozice = pozice
-        self.image = pygame.Surface(rozmer)
+        self.hlava_sprite = "./sprites/head_up.png"
+        self.telo_sprite = "./sprites/body_vertical.png"
+        self.ocas_sprite = "./sprites/tail_up.png"
+        self.image = pygame.image.load(self.hlava_sprite)
         self.rect = self.image.get_rect()
-        self.image.fill(barva)
 
     def zjisteni_smeru(self, stisknute_klavesy):
         if stisknute_klavesy[pygame.K_LEFT] and self.smer_pohybu[0] == 0:
             self.smer_pohybu[0] = -1
             self.smer_pohybu[1] = 0
+            # rotate head sprite
+            self.image = pygame.image.load(self.hlava_sprite)
+            self.image = pygame.transform.rotate(self.image, 90)
         elif stisknute_klavesy[pygame.K_RIGHT] and self.smer_pohybu[0] == 0:
             self.smer_pohybu[0] = 1
             self.smer_pohybu[1] = 0
+            self.image = pygame.image.load(self.hlava_sprite)
+            self.image = pygame.transform.rotate(self.image, -90)
 
         if stisknute_klavesy[pygame.K_UP] and self.smer_pohybu[1] == 0:
             self.smer_pohybu[1] = -1
             self.smer_pohybu[0] = 0
+            self.image = pygame.image.load(self.hlava_sprite)
+
         elif stisknute_klavesy[pygame.K_DOWN] and self.smer_pohybu[1] == 0:
             self.smer_pohybu[1] = 1
             self.smer_pohybu[0] = 0
+            self.image = pygame.image.load(self.hlava_sprite)
+            self.image = pygame.transform.rotate(self.image, 180)
 
     def pohyb(self):
         self.pozice[0] += self.smer_pohybu[0] * self.rozmer[0]
@@ -80,7 +89,7 @@ okno = pygame.display.set_mode((OKNO_SIRKA, OKNO_VYSKA))
 # Nastavení titulku okna
 pygame.display.set_caption("Můj první program v Pygame")
 
-H1 = Snake((50, 50), [100, 100], MODRA, CERNA)
+H1 = Snake((50, 50), [100, 100])
 snake_group = pygame.sprite.Group()
 snake_group.add(H1)
 
