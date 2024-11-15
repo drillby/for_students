c7b2b04c013a5364648442817bd989c229e8fa68
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2024-11-15 12:18:43 +0100

    Update truhla.py

--- EPA_2022/sprite/truhla.py
+++ EPA_2022/sprite/truhla.py
@@ -1,3 +1,9 @@
+# generovat nové mince každých x sekund
+# vypsat skóre
+# max 10 mincí na obrazovce
+
+import math
+
 import pygame as pg
 
 pg.init()
@@ -9,6 +15,9 @@ BILA = (255, 255, 255)
 
 okno = pg.display.set_mode((OKNO_SIRKA, OKNO_VYSKA))
 pg.display.set_caption("Truhla")
+
+hodiny = pg.time.Clock()
+FPS = 60
 
 
 class GameObject(pg.sprite.Sprite):
@@ -29,6 +38,42 @@ class Hrac(GameObject):
     def __init__(self, x, y, rychlost, obrazek, scale):
         super().__init__(x, y, obrazek, scale)
         self.rychlost = rychlost
+
+    def update(self):
+        self.pohyb()
+        self.zkontroluj_hranice()
+
+    def pohyb(self):
+        keys = pg.key.get_pressed()
+        move_x = 0
+        move_y = 0
+
+        if keys[pg.K_LEFT]:
+            move_x = -self.rychlost
+        if keys[pg.K_RIGHT]:
+            move_x = self.rychlost
+        if keys[pg.K_UP]:
+            move_y = -self.rychlost
+        if keys[pg.K_DOWN]:
+            move_y = self.rychlost
+
+        direction = [move_x, move_y]
+        if abs(direction[0]) == self.rychlost and abs(direction[1]) == self.rychlost:
+            direction[0] *= 1 / math.sqrt(2)
+            direction[1] *= 1 / math.sqrt(2)
+
+        self.rect.x += direction[0]
+        self.rect.y += direction[1]
+
+    def zkontroluj_hranice(self):
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > OKNO_SIRKA - self.rect.width:
+            self.rect.x = OKNO_SIRKA - self.rect.width
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > OKNO_VYSKA - self.rect.height:
+            self.rect.y = OKNO_VYSKA - self.rect.height
 
 
 class Mince(GameObject):
@@ -56,8 +101,14 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
+    hrac_group.update()
+
+    if pg.sprite.spritecollide(H1, mince_group, True):
+        print("Mince sebrána!")
+
     okno.fill(BILA)
     hrac_group.draw(okno)
     mince_group.draw(okno)
     pg.display.flip()
+    hodiny.tick(FPS)
 pg.quit()
