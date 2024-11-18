5be7c1f52c7b54cd18dde94e2e1ceae52ba0091b
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2024-11-18 09:41:10 +0100

    Update kalkulacka_pytq.py

--- IT_2022/kalkulacka_pytq.py
+++ IT_2022/kalkulacka_pytq.py
@@ -1,3 +1,5 @@
+# dostat znak z stisknutého tlačítka na QLine
+
 from PyQt5.QtWidgets import (
     QApplication,
     QPushButton,
@@ -31,6 +33,47 @@ class Kalkukacka(QWidget):
             for y, znak in enumerate(radek):
                 tlacitko = QPushButton(znak)
                 self.grid.addWidget(tlacitko, x, y)
+                tlacitko.clicked.connect(self.zpracuj_znak)
+
+
+    def zpracuj_znak(self):
+        tlacitko = self.sender()
+        znak = tlacitko.text()
+        priklad = self.radek.text()
+        if znak == "=":
+            self.vypocti_priklad(priklad, self.radek)
+        else:
+            self.radek.setText(priklad+znak)
+
+
+    def vypocti_priklad(self, priklad, out_widget):
+        povolene_znaky = "0123456789+-*/()"
+        for znak in priklad:
+            if not (znak in povolene_znaky):
+                out_widget.setText("Nepovolený znak")
+                return
+
+        prvni_znak = priklad[0]
+        posledni_znak = priklad[-1]
+        if prvni_znak in "*/" or posledni_znak in "+-*/":
+            out_widget.setText("Nepovolená sekvence znaků")
+            return
+
+        zasobnik = []
+        for znak in priklad:
+            if znak == "(":
+                zasobnik.append("(")
+            elif znak == ")":
+                if not zasobnik:
+                    out_widget.setText("Chybí '('")
+                    return
+                zasobnik.pop()
+
+        if zasobnik:
+            out_widget.setText("Chybí ')")
+            return
+        vysledek = str(eval(priklad))
+        out_widget.setText(vysledek)
 
 
 
