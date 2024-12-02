9f31fa9586f9b2ef4c435f459a644abb6a0e8c39
Pavel Podrazký <77438156+drillby@users.noreply.github.com>
2024-12-02 09:10:11 +0100

    Update password_manager.py

--- IT_2022/password_manager.py
+++ IT_2022/password_manager.py
@@ -1,10 +1,15 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QListWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QListWidget, QPushButton, QLabel, QListWidgetItem
+import json
 
 class PasswordManager(QWidget):
     def __init__(self):
         super().__init__()
+        self.passwords = self.load_passwords("./hesla.json")
         self.initUI()
+
+    def load_passwords(self, path):
+        with open(path, "r") as f:
+            return json.load(f)
 
     def initUI(self):
         self.setWindowTitle("Seznam hesel")
@@ -14,6 +19,13 @@ class PasswordManager(QWidget):
 
         # List hesel
         self.list_widget = QListWidget()
+        for login_pair in self.passwords:
+            username = login_pair["username"] # z json dostanem username
+            password = login_pair["password"] # z json dostanem password
+            # pro každý username, heslo pair vytvoříme ListItem
+            item = QListWidgetItem(f"Username: {username}")
+            self.list_widget.addItem(item)
+            # ještě přidáme funkci pro zobrazení detailu
         main_layout.addWidget(self.list_widget)
 
         # Horizontální layout pro vstupní pole
@@ -30,7 +42,7 @@ class PasswordManager(QWidget):
         password_layout = QHBoxLayout()
         password_label = QLabel("Heslo:")
         self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)  # Zobrazí heslo jako hvězdičky
+        # self.password_input.setEchoMode(QLineEdit.Password)  # Zobrazí heslo jako hvězdičky
         password_layout.addWidget(password_label)
         password_layout.addWidget(self.password_input)
 
